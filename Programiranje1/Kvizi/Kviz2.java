0bd28cd7b77d61cdd3bde835f1c62012e4679c38
vexane <ahacsh@gmail.com>
2021-11-03 16:38:52 +0100

    f

---
+++ Programiranje1/Kvizi/Kviz2.java
@@ -0,0 +1,9 @@
+public class Kviz2 {
+    public static void main(String[] args) {
+        long stevec = 5000000000L;
+        while (stevec != -1) {
+            System.out.println(stevec);
+            stevec--;
+        }
+    }
+}
